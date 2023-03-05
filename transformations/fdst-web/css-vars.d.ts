b2f7d5166a2c8d0cc20e191405c8c7b2613990d5
Scott Rouse <scott.rouse@gmail.com>
2023-03-05 10:34:10 -0600

    Create file: "transformations/fdst-web/css-vars.d.ts"

---
+++ transformations/fdst-web/css-vars.d.ts
@@ -0,0 +1,95 @@
+export type RDSColorValue =
+  'color-primary-1' |
+  'color-primary-2' |
+  'color-primary-3' |
+  'color-primary-4' |
+  'color-primary-5' |
+  'color-primary' |
+  'color-primary-6' |
+  'color-primary-7' |
+  'color-primary-8' |
+  'color-primary-9' |
+  'color-grey-0' |
+  'color-grey-1' |
+  'color-grey-2' |
+  'color-grey-3' |
+  'color-grey-4' |
+  'color-grey-5' |
+  'color-grey' |
+  'color-grey-6' |
+  'color-grey-7' |
+  'color-grey-8' |
+  'color-grey-9' |
+  'color-grey-10';
+
+export type RDSSpacingValue =
+  'spacing-col-0' |
+  'spacing-col-0-3' |
+  'spacing-col-0-4' |
+  'spacing-col-0-6' |
+  'spacing-col-0-8' |
+  'spacing-col-0-9' |
+  'spacing-col-1' |
+  'spacing-col-1-3' |
+  'spacing-col-1-4' |
+  'spacing-col-1-6' |
+  'spacing-col-1-8' |
+  'spacing-col-1-9' |
+  'spacing-col-2' |
+  'spacing-col-2-3' |
+  'spacing-col-2-4' |
+  'spacing-col-2-6' |
+  'spacing-col-2-8' |
+  'spacing-col-2-9' |
+  'spacing-col-3' |
+  'spacing-col-3-3' |
+  'spacing-col-3-4' |
+  'spacing-col-3-6' |
+  'spacing-col-3-8' |
+  'spacing-col-3-9' |
+  'spacing-col-4' |
+  'spacing-col-5' |
+  'spacing-col-6' |
+  'spacing-col-7' |
+  'spacing-col-8' |
+  'spacing-col-9' |
+  'spacing-col-10' |
+  'spacing-col-11' |
+  'spacing-col-12' |
+  'spacing-colvw-0' |
+  'spacing-colvw-0-3' |
+  'spacing-colvw-0-4' |
+  'spacing-colvw-0-6' |
+  'spacing-colvw-0-8' |
+  'spacing-colvw-0-9' |
+  'spacing-colvw-1' |
+  'spacing-colvw-1-3' |
+  'spacing-colvw-1-4' |
+  'spacing-colvw-1-6' |
+  'spacing-colvw-1-8' |
+  'spacing-colvw-1-9' |
+  'spacing-colvw-2' |
+  'spacing-colvw-2-3' |
+  'spacing-colvw-2-4' |
+  'spacing-colvw-2-6' |
+  'spacing-colvw-2-8' |
+  'spacing-colvw-2-9' |
+  'spacing-colvw-3' |
+  'spacing-colvw-3-3' |
+  'spacing-colvw-3-4' |
+  'spacing-colvw-3-6' |
+  'spacing-colvw-3-8' |
+  'spacing-colvw-3-9' |
+  'spacing-colvw-4' |
+  'spacing-colvw-5' |
+  'spacing-colvw-6' |
+  'spacing-colvw-7' |
+  'spacing-colvw-8' |
+  'spacing-colvw-9' |
+  'spacing-colvw-10' |
+  'spacing-colvw-11' |
+  'spacing-colvw-12';
+
+export type RDSValue =
+  RDSColorValue |
+  RDSSpacingValue;
